764231b19e852f3b420b2d13e035ad58d08aa5b9
André Emanuel Cajús de Carvalho <andre-e-carvalho@telecom.pt>
2020-07-30 00:00:26 +0100

    static

--- execute-docker-dev.sh
+++ execute-docker-dev.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 
 python3.8 manage.py migrate
-echo "yes" | python3.8 manage.py collectstatic #validar sem isto
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('useradmin', 'admin@gmail.com', 'teste123?Aa')" | python3.8 manage.py shell
 DJANGO_SUPERUSER_PASSWORD="teste123?Aa"
 python3.8 manage.py createsuperuser --no-input --username="useradmin" --email="my_user@domain.com"
